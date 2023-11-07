ae43416085170f2dda864c8faa00aac538015f30
MarshRey <marshallreynolds2002@gmail.com>
2023-11-07 18:17:28 -0500

    TextToSpeech change
    changed to be a function so other files can call it

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,15 +1,23 @@
-# run pip3 install gTTS
+# run pip3 install gTTS to install the gTTS library
 from gtts import gTTS
 
-# Input text
-text = "Hello, this is your mom who is gay"
+def text_to_speech(text, output_file):
+    # Initialize the gTTS object
+    tts = gTTS(text)
+    
+    # Save the speech as an MP3 file
+    try:
+        tts.save(output_file)
+        print("File saved successfully")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-# Initialize the gTTS object
-tts = gTTS(text)
+if __name__ == "__main__":
+    # Input text
+    text = "Hello, this is your mom who is gay"
 
-# Specify the output file name
-output_file = "output.mp3"
-
-# Save the speech as an MP3 file
-tts.save(output_file)
-
+    # Specify the output file name
+    output_file = "output.mp3"
+    
+    # Call the function
+    text_to_speech(text, output_file)
